d795a8338c01cd2a74275e60725b7c55f31756ce
Sergio Ribezzi <Sergio@MacBook-Air-di-Sergio.local>
2015-11-25 18:49:00 +0100

    Initial Commit

---
+++ DailyPulseApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DailyPulseApp
+//
+//  Created by Sergio Ribezzi on 25/11/15.
+//  Copyright © 2015 DEK. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
